f31da306b4e51197afb067a0d4650cc4b8edf516
Nitesh <nitesh131105@gmail.com>
2026-04-03 21:46:28 +0530

    [wrapper-classes-assignment-no-14]

--- String/src/LC58.java
+++
@@ -1,7 +0,0 @@
-public class LC58 {
-    public int lengthOfLastWord(String s) {
-        String[] arr =s.split(" ");
-        String last= arr[arr.length-1];
-        return last.length();
-    }
-}
--- constructors/src/com/gla/Polymorphism/Inhertance/Animal.java
+++
@@ -1,9 +0,0 @@
-package com.gla.Polymorphism.Inhertance;
-
-public class Animal
-{
-    public void walk()
-    {
-        System.out.println("walks on four Legs.");
-    }
-}
